8f1369e0eca4e7af1510fc902775166ac95dc371
Gregory <gnkoshelev@gmail.com>
2018-03-22 16:41:06 +0500

    Revert original VectorAlgebra.Vector class (with final modifier on fields).

--- src/main/java/ru/gnkoshelev/jbreak2018/perf_tests/vector/VectorAlgebra.java
+++ src/main/java/ru/gnkoshelev/jbreak2018/perf_tests/vector/VectorAlgebra.java
@@ -22,7 +22,7 @@ public class VectorAlgebra {
     }
 
     public final static class Vector {
-        private double x, y, z;
+        private final double x, y, z;
 
         public Vector(double x, double y, double z) {
             this.x = x; this.y = y; this.z = z;
